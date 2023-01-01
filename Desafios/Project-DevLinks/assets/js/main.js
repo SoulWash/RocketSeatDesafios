2b5d5df64c5b0a4527cf66e6a9bb6d34db4da740
Washington Luiz De Sousa <washluiz_@hotmail.com>
2023-01-01 00:40:56 -0300

    Finalizando DevLinks

---
+++ Desafios/Project-DevLinks/assets/js/main.js
@@ -0,0 +1,22 @@
+
+function toggleMode(){
+    const html = document.documentElement;
+    html.classList.toggle('light');
+/* 
+    if (html.classList.contains('light')) {
+        html.classList.remove('light');
+    } else {
+        html.classList.add('light');
+    }
+*/
+
+const img = document.querySelector('#profile img');
+
+    if (html.classList.contains('light')) {
+        img.setAttribute('src', './assets/image/Ellipse.png');
+        img.setAttribute('alt', 'Foto de Washington Luiz com olhos laser');
+    } else {
+        img.setAttribute('src', './assets/image/Avatar.png');
+        img.setAttribute('alt', 'Foto de Washington Luiz');
+    }
+}
